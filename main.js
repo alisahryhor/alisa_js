cf869ffbf87b7d1a5c5fa8ed109611bdd43f4906
alisa_hryhor <alisa.hryhor@forforce.com>
2021-08-07 18:29:03 +0300

    dz_05.08

---
+++ main.js
@@ -0,0 +1,46 @@
+//1 скрин
+
+let langName = prompt('Каково "официальное" название Javascript?');
+
+    if (langName == 'ECMAScript') {
+        alert('Верно!');}
+    else {
+        alert('Не знаете? "EcmaScript"!');
+    }
+
+//2 скрин
+
+/*let test = prompt();
+
+    if (test > 0) {
+        alert(1);
+    }
+    else if (test < 0) {
+            alert(-1);
+        }
+    else  {
+        alert(0);
+    }*/
+
+//3 скрин
+
+/*let login = prompt('Кто пришел?');
+
+    if (login == 'Админ') {
+        prompt('Пароль?');
+        if (login == 'Черный Властелин') {
+            alert('Добро пожаловать!');
+        }
+        else if (login !== 'Черный Властелин') {
+            alert('Пароль неверен');
+        }
+        else {
+            alert('Вход отменен');
+        }
+    }
+    else if (login !== 'Админ') {
+            alert('Я вас не знаю');
+        }
+        else {
+            alert('Вход отменен');
+        }*/
