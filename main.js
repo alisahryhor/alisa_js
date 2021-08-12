481de5c1042957d25f90a7c032a5098ab6b7bf74
alisa_hryhor <alisa.hryhor@forforce.com>
2021-08-12 22:09:38 +0300

    dz_10.08

---
+++ main.js
@@ -0,0 +1,16 @@
+// 1 скрин
+ function buttonClick() {
+     var i = 0;
+     while (i < 3) {
+        alert('номер' + i + '!');
+        i++;
+     }
+ }
+
+// 2 скрин
+function buttonClick() {
+    let num = 0;
+        do {
+            num = prompt('Введите число больше 100');
+        } while (num <= 100);
+    }
